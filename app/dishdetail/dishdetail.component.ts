c16d89deb5ecc2682fa93780e3d9dcbc59361044
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-15 13:54:34 +0530

    Toasty

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -6,7 +6,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { FavoriteService } from '../services/favorite.service';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import { Toasty } from 'nativescript-toasty';
 import 'rxjs/add/operator/switchMap';
+
 
 @Component({
     selector: 'app-dishdetail',
@@ -49,8 +51,8 @@ export class DishdetailComponent implements OnInit {
         if (!this.favorite) {
             console.log('Adding to Favorites', this.dish.id);
             this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-            //const toast = new Toasty("Added Dish " + this.dish.id, "short", "bottom");
-            //toast.show();
+            const toast = new Toasty("Added Dish " + this.dish.id, "short", "bottom");
+            toast.show();
         }
     }
 
