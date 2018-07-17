724d9a6b0b15a40384b367b20c6bdede14738b8f
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-17 16:18:23 +0530

    Social Sharing

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -17,6 +17,8 @@ import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
 import { Color } from 'color';
 import * as enums from "ui/enums";
 import 'rxjs/add/operator/switchMap';
+import * as SocialShare from "nativescript-social-share";
+import { ImageSource, fromUrl } from "image-source";
 
 
 @Component({
@@ -79,7 +81,7 @@ export class DishdetailComponent implements OnInit {
             title: "More Options",
             message: "Perform an Action",
             cancelButtonText: "Cancel",
-            actions: ["Add to Favorites", "Add Comment"]
+            actions: ["Add to Favorites", "Add Comment", "Social Sharing"]
         };
 
         action(options).then((result) => {
@@ -87,6 +89,8 @@ export class DishdetailComponent implements OnInit {
                 this.addToFavorites();
             } else if (result === "Add Comment") {
                 this.createModalView();
+            } else if (result === 'Social Sharing') {
+                this.socialShare();
             }
         });
     }
@@ -197,4 +201,16 @@ export class DishdetailComponent implements OnInit {
                 console.log(e.message);
             });
     }
+
+    socialShare() {
+        let image: ImageSource;
+        fromUrl(this.BaseURL + this.dish.image)
+            .then((img: ImageSource) => {
+                image = img;
+                SocialShare.shareImage(image, "How would you like to share this image?")
+            })
+            .catch(() => { console.log('Error loading image'); });
+    }
+
+
 }
