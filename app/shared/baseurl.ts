e81a5e92d845e5a155dfd7f6959a4591cbc701b6
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-17 21:34:54 +0530

    Updated for Section 4

--- app/shared/baseurl.ts
+++ app/shared/baseurl.ts
@@ -1,2 +1,2 @@
-//export const baseURL = "http://192.168.2.11:3000/";
-export const baseURL = "http://172.20.10.5:3000/";
+export const baseURL = "http://192.168.2.11:3000/";
+// export const baseURL = "http://172.20.10.5:3000/";
