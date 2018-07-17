5ce1a5f5aba2a54db23dfcc98fbc36abbbeb944d
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-17 15:25:23 +0530

    Sending Emai

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DrawerPage } from '../shared/drawer/drawer.page';
 import * as app from "application";
 import { RadSideDrawer } from 'nativescript-telerik-ui/sidedrawer';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
 
 @Component({
     selector: 'app-contact',
@@ -10,16 +12,32 @@ import { RadSideDrawer } from 'nativescript-telerik-ui/sidedrawer';
 })
 export class ContactComponent extends DrawerPage implements OnInit {
 
-    constructor(private changeDetectorRef: ChangeDetectorRef) {
+    constructor(private changeDetectorRef: ChangeDetectorRef,
+        private fonticon: TNSFontIconService) {
         super(changeDetectorRef);
     }
 
     ngOnInit() {
-        
+
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    sendEmail() {
+        Email.available()
+            .then((avail: boolean) => {
+                if (avail) {
+                    Email.compose({
+                        to: ['confusion@food.net'],
+                        subject: '[ConFusion]: Query',
+                        body: 'Dear Sir/Madam:'
+                    });
+                }
+                else
+                    console.log('No Email Configured');
+            })
     }
 }
